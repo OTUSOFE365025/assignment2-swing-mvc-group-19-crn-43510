0fd8ad83b331165f71ea9067fd428499edd739eb
neha <neha.khan1@ontariotechu.net>
2025-10-08 21:52:16 -0400

    add code files

--- SwingMVC/src/MySwingMVCApp.java
+++ SwingMVC/src/MySwingMVCApp.java
@@ -1,17 +1,17 @@
-
 public class MySwingMVCApp {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		  // Assemble all the pieces of the MVC
-		  Model m = new Model("Sylvain", "Saurel");
-		  View v = new View("MVC with SSaurel");
-		  Controller c = new Controller(m, v);
-		  
-		  // create scanner
-		  Scanner scanner = new Scanner();
-		  
-		  c.initController();
-	}
+		Model m = new Model("Sylvain", "Saurel");
+		View v = new View("MVC with SSaurel");
+		Controller c = new Controller(m, v);
+		c.initController();
 
+		String productFile = "products.txt";
+		CashRegister cashRegister = new CashRegister(productFile);
+
+
+		c.setCashRegister(cashRegister, v);
+		Scanner scanner = new Scanner(c);
+
+		System.out.println("Application started!");
+	}
 }
